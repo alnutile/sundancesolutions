f796a52c0ddad01c614f425a8e57518ad0febef3
Alfred Nutile <365385+alnutile@users.noreply.github.com>
2022-07-15 10:07:33 -0400

    add phone

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -25,13 +25,32 @@
                 <div class="absolute inset-0 bg-gray-800 mix-blend-multiply" aria-hidden="true"></div>
             </div>
             <div class="relative max-w-7xl mx-auto py-24 px-4 sm:py-32 sm:px-6 lg:px-8">
-                <h1 class="text-4xl font-extrabold tracking-tight text-white md:text-5xl lg:text-6xl">Sundance
+                <div class="text-center mb-6">
+                    <div>
+                    <h1 class="text-4xl font-extrabold tracking-tight text-white md:text-5xl lg:text-6xl">Sundance
                     Solutions,
-                    LLC</h1>
-                <p class="mt-6 max-w-3xl text-xl text-gray-300">
+                    LLC</h1></div>
+                <div class="mt-6 text-xl text-gray-300 text-center">
                     Your internal applications or Saas Products built right and built with Laravel.
-                    info@sundancesolutions.io
-                </p>
+                </div>
+                </div>
+                <div class="grid grid-cols-1 sm:grid-cols-2 justify-evenly text-white mb-4">
+                    <div class="flex justify-center">
+                        <div class="mr-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4" />
+                            </svg>
+                        </div>
+                        <div>info@sundancesolutions.io</div>
+                    </div>
+                    <div class="flex justify-center mt-4 sm:mt-0">
+                        <div class="mr-2"><svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
+                            </svg>
+                        </div>
+                        <div>+1(413)-225-1844</div>
+                    </div>
+                </div>
             </div>
         </div>
 
